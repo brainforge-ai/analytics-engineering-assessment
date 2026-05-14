d2844fd3bcadf3b1cb75e700722903510bbcab93
Austin Whittaker <akwhit7@gmail.com>
2026-05-14 11:28:14 -0400

    add schema.yml for test documentation

--- tests/test_fct_monthly_revenue_no_nulls.sql
+++ tests/test_fct_monthly_revenue_no_nulls.sql
@@ -2,7 +2,10 @@ select
     country
     ,currency
     ,year_month
-    ,
+    ,latest_updated_at
+    ,total_revenue
+    ,order_count
+    ,avg_order_value
 from 
     {{ref('fct_monthly_revenue')}}
 where 
